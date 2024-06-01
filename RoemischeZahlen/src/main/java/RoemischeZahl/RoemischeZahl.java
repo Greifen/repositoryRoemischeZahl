a946810510d95e7f6d1dc98c793636f67ecfd70a
karin <karin@DESKTOP-JFTOVL2>
2024-06-01 16:07:18 +0200

    dezimalzahlInRoemische (50 min, 4 mal debugger gebraucht, hoch lebe der debugger)

--- RoemischeZahlen/src/main/java/RoemischeZahl/RoemischeZahl.java
+++ RoemischeZahlen/src/main/java/RoemischeZahl/RoemischeZahl.java
@@ -1,28 +1,40 @@
 package RoemischeZahl;
 
+
 public class RoemischeZahl {
 
 	private char[] roemischeZahlen = { 'I', 'V', 'X', 'L', 'C', 'D', 'M' };
-	private int[] roemischeZahlenWerte = { 1, 5, 10, 50, 100, 500, 1000 };
+	private int[] roemischeZahlenWerte = { 1, 5, 10, 50, 100, 500, 1000};
 
 	public static void main(String[] args) {
 		String roemischeZahl1 = "CMXC";
 		String roemischeZahl2 = "CMLXXVI";
-		RoemischeZahl roemischeZahlen = new RoemischeZahl();
-		int dezimalzahl1 = roemischeZahlen.roemischeZahlInDezimal(roemischeZahl1);
-		int dezimalzahl2 = roemischeZahlen.roemischeZahlInDezimal(roemischeZahl2);
+		RoemischeZahl roemischeZahle = new RoemischeZahl();
+		int dezimalzahl1 = roemischeZahle.roemischeZahlInDezimal(roemischeZahl1);
+		int dezimalzahl2 = roemischeZahle.roemischeZahlInDezimal(roemischeZahl2);
+		System.out.println(dezimalzahl1);
+		System.out.println(dezimalzahl2);
 		System.out.println(dezimalzahl1 + dezimalzahl2);
+		System.out.println(roemischeZahle.dezimalzahlInRoemische(dezimalzahl1 + dezimalzahl2));
+		
+//		//wie konkatiniere ich String mit char?
+//		String hallo="hallo";
+//		char h='h';
+//		char[] roemischeZahlen2 = { 'I', 'V', 'X', 'L', 'C', 'D', 'M' };
+//		hallo += roemischeZahlen2[0];
+//		hallo += roemischeZahlen2[1];
+//		System.out.println(hallo);
 	}
 
 	/**
-	 * wandelt roemische Zahl in eine Dezimalzahl um. TODO: i==j+1 ||i==j-1
+	 * wandelt eine roemische Zahl in eine Dezimalzahl um. TODO: i==j+1 ||i==j-1
 	 * Überprüfung der römischen Zahl selbst (eine kleinere vor und nur 3 kleinere
 	 * nach; allerdings 4 Zeichen speichern)
 	 * 
 	 * @param roemischeZahl
 	 * @return dezimalZahl
 	 */
-	private int roemischeZahlInDezimal(String roemischeZahl) {
+	public int roemischeZahlInDezimal(String roemischeZahl) {
 		int dezimalzahl = 0;
 		char zeichen1;
 		char zeichen2;
@@ -40,18 +52,16 @@ public class RoemischeZahl {
 			} else {
 				dezimalzahl += roemischeZahlenWerte[stelleInArraysZeichen1];
 			}
-
-			System.out.println(dezimalzahl);
+//			TODO:logging
+//			System.out.println(dezimalzahl);
 		}
 
 		dezimalzahl += roemischeZahlenWerte[stelleInArraysZeichen2];
-		System.out.println(dezimalzahl);
 		return dezimalzahl;
 	} 
 
 	/**
-	 * gibt die Stelle in den Arrays roemischeZahlen bzw. roemischeZahlenWerte
-	 * zurück.
+	 * wandelt eine Dezimalzahl in eine roemische Zahl um.
 	 * 
 	 * @param zeichen
 	 * @return stelleInArray
@@ -65,4 +75,50 @@ public class RoemischeZahl {
 		return stelleInArrays;
 	}
 
+	/**
+	 * gibt die Stelle in den Arrays roemischeZahlen bzw. roemischeZahlenWerte
+	 * zurück.
+	 * 
+	 * @param dezimalzahl
+	 * @return roemische Zahl
+	 */
+	public String dezimalzahlInRoemische(int dezimalzahl) {
+		String roemischeZahl="";
+		int anzahlZehnerpotzen;
+		for (int stelleInArrays = roemischeZahlenWerte.length-1; stelleInArrays>=0 ; stelleInArrays-=2) {
+			anzahlZehnerpotzen=dezimalzahl/roemischeZahlenWerte[stelleInArrays];
+			dezimalzahl-=anzahlZehnerpotzen*roemischeZahlenWerte[stelleInArrays];
+			if(stelleInArrays!=6) {
+				if(anzahlZehnerpotzen!=0) {
+					if (anzahlZehnerpotzen<4) {
+						for (int i = 0; i < anzahlZehnerpotzen; i++) {
+							roemischeZahl += roemischeZahlen[stelleInArrays];
+						}
+					}
+					else if(anzahlZehnerpotzen==4) {
+						roemischeZahl += roemischeZahlen[stelleInArrays];
+						roemischeZahl += roemischeZahlen[stelleInArrays+1];
+					}
+					else if(anzahlZehnerpotzen==5) {
+						roemischeZahl += roemischeZahlen[stelleInArrays+1];
+					}
+					else if(anzahlZehnerpotzen<9)
+					{
+						roemischeZahl += roemischeZahlen[stelleInArrays+1];
+						for (int i = 0; i < anzahlZehnerpotzen-5; i++) {
+							roemischeZahl += roemischeZahlen[stelleInArrays];
+						}
+					}
+					else { //if(x==9)
+						roemischeZahl += roemischeZahlen[stelleInArrays];
+						roemischeZahl += roemischeZahlen[stelleInArrays+2];
+					}
+				}
+			}
+			else {
+				System.err.println("Dezimalzahl muss unter 3899 sein.");
+			}
+		}
+		return roemischeZahl;
+	}
 }
